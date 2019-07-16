a206e61c7799058cd37e67f83a2b6c2cfc638ac0
Patrick Kim <pkim050@gmail.com>
2019-07-16 16:00:15 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -3,4 +3,11 @@ import React, {Component} from React;
 
 export default class DelayedButton extends Component {
 
+  render() {
+    return (
+      <div>
+        <button onClick={this.clickEvent}>Button</button>
+      </div>
+    )
+  }
 }
